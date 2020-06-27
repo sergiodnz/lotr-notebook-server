49d8efdb10e4f693554635a085453581c6977d6d
Claudio Romero <romerolrocha@gmail.com>
2020-06-27 16:23:07 -0300

    Create express server

--- src/server.js
+++ src/server.js
@@ -3,4 +3,12 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-console.log(process.env.API_TOKEN);
+const PORT = 5000;
+const RIDDLE = `Listening at the Gates of Moria (http://localhost:${PORT}). Speak your token, and enter.`;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(PORT, () => console.log(RIDDLE));
