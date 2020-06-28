ffa81366504ca1cb8f1c7ddd36ab8814b7bc785a
Claudio Romero <romerolrocha@gmail.com>
2020-06-27 22:33:38 -0300

    Add auth constraints

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,11 +1,36 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'https://the-one-api.herokuapp.com/v1',
-  headers: {
-    Authorization: `Bearer ${process.env.API_TOKEN}`,
+let apiToken = '';
+
+export const authMiddleware = (req, res, next) => {
+  const token = req.get('Authorization');
+  if (token) {
+    apiToken = token;
+    next();
+  } else {
+    res.status(403).send({
+      error: `The Doors of Durin, Lord of Moria. Speak "Authorization Header" and enter.`,
+    });
+  }
+};
+
+const instance = axios.create({ timeout: 1000 });
+
+instance.interceptors.request.use(
+  config => {
+    config.baseURL = 'https://the-one-api.herokuapp.com/v1';
+    config.headers['Authorization'] = apiToken;
+    return config;
   },
-  timeout: 1000,
-});
+  error => Promise.reject(error)
+);
+
+instance.interceptors.response.use(
+  res => res,
+  err => {
+    console.log(err);
+    return Promise.reject(err);
+  }
+);
 
 export default instance;
--- src/server.js
+++ src/server.js
@@ -3,19 +3,22 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+import { authMiddleware } from './config/axios';
 import routes from './routes';
 
-const PORT = 5000;
-const RIDDLE = `Listening at the Gates of Moria (http://localhost:${PORT}). Speak your token, and enter.`;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(authMiddleware);
 
 app.use('/character', routes.character);
 app.use('/quote', routes.quote);
 app.use('/book', routes.book);
 app.use('/movie', routes.movie);
 
-app.listen(PORT, () => console.log(RIDDLE));
+const PORT = 5000;
+const LISTENING_MSG = `In a hole in the ground there lived a Hobbit --> http://localhost:${PORT}`;
+
+app.listen(PORT, () => console.log(LISTENING_MSG));
