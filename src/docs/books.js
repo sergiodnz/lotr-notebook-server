d2beae3c98ab1934f765dbc7e085e15754ecd2dd
Claudio Romero <romerolrocha@gmail.com>
2020-06-28 15:34:14 -0300

    Finish 'books' API docs

--- src/docs/books.js
+++ src/docs/books.js
@@ -1,3 +1,46 @@
-export const schema = {};
+import { getResponse } from './util';
 
-export const paths = {};
+export const schema = {
+  Book: {
+    type: 'object',
+    properties: {
+      _id: { type: 'string' },
+      name: { type: 'string' },
+    },
+  },
+  Books: {
+    type: 'array',
+    items: {
+      $ref: '#/components/schemas/Book',
+    },
+  },
+};
+
+export const paths = {
+  '/books': {
+    get: {
+      tags: ['books'],
+      description: 'Get a list of LOTR books',
+      responses: {
+        '200': getResponse('List of LOTR books', 'Books'),
+      },
+    },
+  },
+  '/books/{id}': {
+    get: {
+      tags: ['books'],
+      description: 'Get all details of a book',
+      parameters: [
+        {
+          name: 'id',
+          in: 'path',
+          required: true,
+          description: '_id of the book',
+        },
+      ],
+      responses: {
+        '200': getResponse('Book Details', 'Book'),
+      },
+    },
+  },
+};
--- src/lotrapi/book.js
+++ src/lotrapi/book.js
@@ -1,10 +1,7 @@
 import axios from '../config/axios';
 
 export const getAll = () => {
-  return axios.get('/book').then(res => {
-    const { docs, items } = res.data;
-    return { data: docs, total: items.total };
-  });
+  return axios.get('/book').then(res => res.data.docs);
 };
 
 export const getByID = id => {
